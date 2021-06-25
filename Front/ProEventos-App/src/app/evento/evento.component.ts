e8e9795040348d6ea9d80d6352872310f3ea6889
Gabriel Duarte <gabriel.silvaduarte@hotmail.com>
2021-06-25 09:18:35 -0300

    Seção 4: Angular

---
+++ Front/ProEventos-App/src/app/evento/evento.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evento',
+  templateUrl: './evento.component.html',
+  styleUrls: ['./evento.component.scss']
+})
+export class EventoComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void{
+    this.http.get('https://localhost:5001/api/evento').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+  }
+
+}
